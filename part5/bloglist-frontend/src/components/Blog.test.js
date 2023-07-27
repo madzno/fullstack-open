5530c7b112b8cda72834b182e2678350f0009aa5
Madz No <madzno12@gmail.com>
2023-07-27 15:20:59 -0400

    adding front end tests

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,3 +1,16 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders content', () => {
+  const blog = {
+    title: 'new blog',
+    author: 'John Smith'
+  }
+
+  render(<Blog blog={blog} />)
+
+  const element = screen.getByText('new blog', { exact: false })
+  expect(element).toBeDefined()
+})
